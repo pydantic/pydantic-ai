663c984fdc2ee2096a433be8bdb58b143ba3835d
Samuel Colvin <s@muelcolvin.com>
2024-10-28 22:25:58 +0000

    fix tests

--- tests/import_examples.py
+++ tests/import_examples.py
@@ -4,7 +4,10 @@ Used to check that examples are at least valid syntax and can be imported withou
 Called in CI.
 """
 
+import os
 from pathlib import Path
+
+os.environ.update(OPENAI_API_KEY='fake-key', GEMINI_API_KEY='fake-key')
 
 examples_dir = Path(__file__).parent.parent / 'examples'
 for example in examples_dir.glob('*.py'):
