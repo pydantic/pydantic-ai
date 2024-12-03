3d451542744d47f5fa1780bc19b52d2cc212d40b
Samuel Colvin <s@muelcolvin.com>
2024-12-03 18:20:30 +0000

    make more dc fields priviate

--- pydantic_ai_slim/pydantic_ai/tool.py
+++ pydantic_ai_slim/pydantic_ai/tool.py
@@ -29,25 +29,24 @@ class Tool(Generic[AgentDeps]):
     """Name of the tool, inferred from the function if left blank."""
     description: str = ''
     """Description of the tool, inferred from the function if left blank."""
-    is_async: bool = field(init=False)
-    single_arg_name: str | None = field(init=False)
-    positional_fields: list[str] = field(init=False)
-    var_positional_field: str | None = field(init=False)
-    validator: SchemaValidator = field(init=False, repr=False)
-    json_schema: _utils.ObjectJsonSchema = field(init=False)
+    _is_async: bool = field(init=False)
+    _single_arg_name: str | None = field(init=False)
+    _positional_fields: list[str] = field(init=False)
+    _var_positional_field: str | None = field(init=False)
+    _validator: SchemaValidator = field(init=False, repr=False)
+    _json_schema: _utils.ObjectJsonSchema = field(init=False)
     _current_retry: int = field(default=0, init=False)
-    outer_typed_dict_key: str | None = field(default=None, init=False)
 
     def __post_init__(self):
         f = _pydantic.function_schema(self.function, self.takes_ctx)
         self.name = self.name or self.function.__name__
         self.description = self.description or f['description']
-        self.is_async = inspect.iscoroutinefunction(self.function)
-        self.single_arg_name = f['single_arg_name']
-        self.positional_fields = f['positional_fields']
-        self.var_positional_field = f['var_positional_field']
-        self.validator = f['validator']
-        self.json_schema = f['json_schema']
+        self._is_async = inspect.iscoroutinefunction(self.function)
+        self._single_arg_name = f['single_arg_name']
+        self._positional_fields = f['positional_fields']
+        self._var_positional_field = f['var_positional_field']
+        self._validator = f['validator']
+        self._json_schema = f['json_schema']
 
     def reset(self) -> None:
         """Reset the current retry count."""
@@ -57,15 +56,15 @@ class Tool(Generic[AgentDeps]):
         """Run the tool function asynchronously."""
         try:
             if isinstance(message.args, messages.ArgsJson):
-                args_dict = self.validator.validate_json(message.args.args_json)
+                args_dict = self._validator.validate_json(message.args.args_json)
             else:
-                args_dict = self.validator.validate_python(message.args.args_dict)
+                args_dict = self._validator.validate_python(message.args.args_dict)
         except ValidationError as e:
             return self._on_error(e, message)
 
         args, kwargs = self._call_args(deps, args_dict, message)
         try:
-            if self.is_async:
+            if self._is_async:
                 function = cast(Callable[[Any], Awaitable[str]], self.function)
                 response_content = await function(*args, **kwargs)
             else:
@@ -81,17 +80,25 @@ class Tool(Generic[AgentDeps]):
             tool_id=message.tool_id,
         )
 
+    @property
+    def json_schema(self) -> _utils.ObjectJsonSchema:
+        return self._json_schema
+
+    @property
+    def outer_typed_dict_key(self) -> str | None:
+        return None
+
     def _call_args(
         self, deps: AgentDeps, args_dict: dict[str, Any], message: messages.ToolCall
     ) -> tuple[list[Any], dict[str, Any]]:
-        if self.single_arg_name:
-            args_dict = {self.single_arg_name: args_dict}
+        if self._single_arg_name:
+            args_dict = {self._single_arg_name: args_dict}
 
         args = [RunContext(deps, self._current_retry, message.tool_name)] if self.takes_ctx else []
-        for positional_field in self.positional_fields:
+        for positional_field in self._positional_fields:
             args.append(args_dict.pop(positional_field))
-        if self.var_positional_field:
-            args.extend(args_dict.pop(self.var_positional_field))
+        if self._var_positional_field:
+            args.extend(args_dict.pop(self._var_positional_field))
 
         return args, args_dict
 
