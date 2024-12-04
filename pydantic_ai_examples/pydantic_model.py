deb5d5170f6236561174e2a9f223c243a2d30922
Samuel Colvin <s@muelcolvin.com>
2024-12-04 19:27:34 +0000

    adding Agent name

--- pydantic_ai_examples/pydantic_model.py
+++ pydantic_ai_examples/pydantic_model.py
@@ -25,9 +25,9 @@ class MyModel(BaseModel):
 
 model = cast(KnownModelName, os.getenv('PYDANTIC_AI_MODEL', 'openai:gpt-4o'))
 print(f'Using model: {model}')
-agent = Agent(model, result_type=MyModel)
+pydantic_agent = Agent(model, result_type=MyModel)
 
 if __name__ == '__main__':
-    result = agent.run_sync('The windy city in the US of A.')
+    result = pydantic_agent.run_sync('The windy city in the US of A.')
     print(result.data)
     print(result.cost())
