dad2ebb06722fc9a6b16e834ac09ea77d5e6d99f
Arnaud Durand <arnaud.durand@live.com>
2024-12-18 19:42:29 +0100

    Fix error when retreiving messages from DB in chat_app example
    Fix #487

--- pydantic_ai_examples/chat_app.py
+++ pydantic_ai_examples/chat_app.py
@@ -31,9 +31,9 @@ from pydantic_ai.exceptions import UnexpectedModelBehavior
 from pydantic_ai.messages import (
     ModelMessage,
     ModelMessagesTypeAdapter,
+    ModelRequest,
     ModelResponse,
     TextPart,
-    UserPromptPart,
 )
 
 # 'if-token-present' means nothing will be sent (and the example will work) if you don't have logfire configured
@@ -86,14 +86,14 @@ class ChatMessage(TypedDict):
 
 
 def to_chat_message(m: ModelMessage) -> ChatMessage:
-    if isinstance(m, UserPromptPart):
+    first_part = m.parts[0]
+    if isinstance(m, ModelRequest):
         return {
             'role': 'user',
-            'timestamp': m.timestamp.isoformat(),
-            'content': m.content,
+            'timestamp': first_part.timestamp.isoformat(),
+            'content': first_part.content,
         }
     elif isinstance(m, ModelResponse):
-        first_part = m.parts[0]
         if isinstance(first_part, TextPart):
             return {
                 'role': 'model',
