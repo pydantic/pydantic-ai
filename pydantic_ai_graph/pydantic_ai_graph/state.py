be3f689d2fabc34da28dfe8c3d45d3acf412f47f
Samuel Colvin <s@muelcolvin.com>
2025-01-07 22:40:28 +0000

    adding node highlighting to mermaid, testing locally

--- pydantic_ai_graph/pydantic_ai_graph/state.py
+++ pydantic_ai_graph/pydantic_ai_graph/state.py
@@ -3,10 +3,12 @@ from __future__ import annotations as _annotations
 import copy
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import TYPE_CHECKING, Generic, Literal, Union
 
 from typing_extensions import Never, TypeVar
+
+from . import _utils
 
 __all__ = 'AbstractState', 'StateT', 'Step', 'EndEvent', 'StepOrEnd'
 
@@ -35,14 +37,17 @@ class Step(Generic[StateT, RunEndT]):
 
     state: StateT
     node: BaseNode[StateT, RunEndT]
-    start_ts: datetime = field(default_factory=lambda: datetime.now(tz=timezone.utc))
+    start_ts: datetime = field(default_factory=_utils.now_utc)
     duration: float | None = None
 
-    kind: Literal['start_step'] = 'start_step'
+    kind: Literal['step'] = 'step'
 
     def __post_init__(self):
         # Copy the state to prevent it from being modified by other code
         self.state = copy.deepcopy(self.state)
+
+    def node_summary(self) -> str:
+        return str(self.node)
 
 
 @dataclass
@@ -51,13 +56,16 @@ class EndEvent(Generic[StateT, RunEndT]):
 
     state: StateT
     result: End[RunEndT]
-    ts: datetime = field(default_factory=lambda: datetime.now(tz=timezone.utc))
+    ts: datetime = field(default_factory=_utils.now_utc)
 
     kind: Literal['end'] = 'end'
 
     def __post_init__(self):
         # Copy the state to prevent it from being modified by other code
         self.state = copy.deepcopy(self.state)
+
+    def node_summary(self) -> str:
+        return str(self.result)
 
 
 StepOrEnd = Union[Step[StateT, RunEndT], EndEvent[StateT, RunEndT]]
