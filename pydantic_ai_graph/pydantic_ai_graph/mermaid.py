d985db409fb606910f2b4d9469c98362b42de915
Samuel Colvin <s@muelcolvin.com>
2025-01-08 18:07:47 +0000

    simplify mermaid config

--- pydantic_ai_graph/pydantic_ai_graph/mermaid.py
+++ pydantic_ai_graph/pydantic_ai_graph/mermaid.py
@@ -1,12 +1,12 @@
 from __future__ import annotations as _annotations
 
 import base64
-from collections.abc import Sequence
+from collections.abc import Iterable, Sequence
 from pathlib import Path
-from typing import TYPE_CHECKING, Annotated, Any, Literal, cast
+from typing import TYPE_CHECKING, Annotated, Any, Literal
 
 from annotated_types import Ge, Le
-from typing_extensions import TypeAlias
+from typing_extensions import TypeAlias, TypedDict, Unpack
 
 from .nodes import BaseNode
 
@@ -14,27 +14,29 @@ if TYPE_CHECKING:
     from .graph import Graph
 
 
-NodeIdent: TypeAlias = 'type[BaseNode[Any, Any]] | str'
-DEFAULT_HIGHLIGHT_CSS = 'fill:#f9f'
+NodeIdent: TypeAlias = 'type[BaseNode[Any, Any]] | BaseNode[Any, Any] | str'
+DEFAULT_HIGHLIGHT_CSS = 'fill:#fdff32'
 
 
 def generate_code(
     graph: Graph[Any, Any],
-    start_node_ids: set[str],
+    start_nodes: Sequence[NodeIdent] | NodeIdent,
+    /,
     *,
-    highlighted_nodes: Sequence[NodeIdent] | None = None,
+    highlighted_nodes: Sequence[NodeIdent] | NodeIdent | None = None,
     highlight_css: str = DEFAULT_HIGHLIGHT_CSS,
 ) -> str:
     """Generate Mermaid code for a graph.
 
     Args:
         graph: The graph to generate the image for.
-        start_node_ids: Identifiers of nodes that start the graph.
+        start_nodes: Identifiers of nodes that start the graph.
         highlighted_nodes: Identifiers of nodes to highlight.
         highlight_css: CSS to use for highlighting nodes.
 
     Returns: The Mermaid code for the graph.
     """
+    start_node_ids = set(node_ids(start_nodes))
     for node_id in start_node_ids:
         if node_id not in graph.node_defs:
             raise LookupError(f'Start node "{node_id}" is not in the graph.')
@@ -59,8 +61,7 @@ def generate_code(
     if highlighted_nodes:
         lines.append('')
         lines.append(f'classDef highlighted {highlight_css}')
-        for node in highlighted_nodes:
-            node_id = node if isinstance(node, str) else node.get_id()
+        for node_id in node_ids(highlighted_nodes):
             if node_id not in graph.node_defs:
                 raise LookupError(f'Highlighted node "{node_id}" is not in the graph.')
             lines.append(f'class {node_id} highlighted')
@@ -68,82 +69,111 @@ def generate_code(
     return '\n'.join(lines)
 
 
-ImageType = Literal['jpeg', 'png', 'webp', 'svg', 'pdf']
-PdfPaper = Literal['letter', 'legal', 'tabloid', 'ledger', 'a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6']
-Theme = Literal['default', 'neutral', 'dark', 'forest']
+def node_ids(node_idents: Sequence[NodeIdent] | NodeIdent) -> Iterable[str]:
+    """Get the node IDs from a sequence of node identifiers."""
+    if isinstance(node_idents, str):
+        node_iter = (node_idents,)
+    elif isinstance(node_idents, Sequence):
+        node_iter = node_idents
+    else:
+        node_iter = (node_idents,)
+
+    for node in node_iter:
+        if isinstance(node, str):
+            yield node
+        else:
+            yield node.get_id()
+
+
+class MermaidConfig(TypedDict, total=False):
+    """Parameters to configure mermaid chart generation."""
+
+    highlighted_nodes: Sequence[NodeIdent] | NodeIdent
+    """Identifiers of nodes to highlight."""
+    highlight_css: str
+    """CSS to use for highlighting nodes."""
+    image_type: Literal['jpeg', 'png', 'webp', 'svg', 'pdf']
+    """The image type to generate. If unspecified, the default behavior is `'jpeg'`."""
+    pdf_fit: bool
+    """When using image_type='pdf', whether to fit the diagram to the PDF page."""
+    pdf_landscape: bool
+    """When using image_type='pdf', whether to use landscape orientation for the PDF.
+
+    This has no effect if using `pdf_fit`.
+    """
+    pdf_paper: Literal['letter', 'legal', 'tabloid', 'ledger', 'a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6']
+    """When using image_type='pdf', the paper size of the PDF."""
+    background_color: str
+    """The background color of the diagram.
+
+    If None, the default transparent background is used. The color value is interpreted as a hexadecimal color
+    code by default (and should not have a leading '#'), but you can also use named colors by prefixing the
+    value with `'!'`. For example, valid choices include `background_color='!white'` or `background_color='FF0000'`.
+    """
+    theme: Literal['default', 'neutral', 'dark', 'forest']
+    """The theme of the diagram. Defaults to 'default'."""
+    width: int
+    """The width of the diagram."""
+    height: int
+    """The height of the diagram."""
+    scale: Annotated[float, Ge(1), Le(3)]
+    """The scale of the diagram.
+
+    The scale must be a number between 1 and 3, and you can only set a scale if one or both of width and height are set.
+    """
 
 
 def request_image(
     graph: Graph[Any, Any],
-    start_node_ids: set[str],
-    *,
-    highlighted_nodes: Sequence[NodeIdent] | None = None,
-    highlight_css: str = 'fill:#f9f,stroke:#333,stroke-width:4px',
-    image_type: ImageType | str | None = None,
-    pdf_fit: bool = False,
-    pdf_landscape: bool = False,
-    pdf_paper: PdfPaper | None = None,
-    bg_color: str | None = None,
-    theme: Theme | None = None,
-    width: int | None = None,
-    height: int | None = None,
-    scale: Annotated[float, Ge(1), Le(3)] | None = None,
+    start_nodes: Sequence[NodeIdent] | NodeIdent,
+    /,
+    **kwargs: Unpack[MermaidConfig],
 ) -> bytes:
     """Generate an image of a Mermaid diagram using [mermaid.ink](https://mermaid.ink).
 
     Args:
         graph: The graph to generate the image for.
-        start_node_ids: IDs of start nodes of the graph.
-        highlighted_nodes: Identifiers of nodes to highlight.
-        highlight_css: CSS to use for highlighting nodes.
-        image_type: The image type to generate. If unspecified, the default behavior is `'jpeg'`.
-        pdf_fit: When using image_type='pdf', whether to fit the diagram to the PDF page.
-        pdf_landscape: When using image_type='pdf', whether to use landscape orientation for the PDF.
-            This has no effect if using `pdf_fit`.
-        pdf_paper: When using image_type='pdf', the paper size of the PDF.
-        bg_color: The background color of the diagram. If None, the default transparent background is used.
-            The color value is interpreted as a hexadecimal color code by default (and should not have a leading '#'),
-            but you can also use named colors by prefixing the value with `'!'`.
-            For example, valid choices include `bg_color='!white'` or `bg_color='FF0000'`.
-        theme: The theme of the diagram. Defaults to 'default'.
-        width: The width of the diagram.
-        height: The height of the diagram.
-        scale: The scale of the diagram. The scale must be a number between 1 and 3, and you can only set
-            a scale if one or both of width and height are set.
+        start_nodes: Identifiers of nodes that start the graph.
+        **kwargs: Additional parameters to configure mermaid chart generation.
 
     Returns: The image data.
     """
     import httpx
 
-    code = generate_code(graph, start_node_ids, highlighted_nodes=highlighted_nodes, highlight_css=highlight_css)
+    code = generate_code(
+        graph,
+        start_nodes,
+        highlighted_nodes=kwargs.get('highlighted_nodes'),
+        highlight_css=kwargs.get('highlight_css', DEFAULT_HIGHLIGHT_CSS),
+    )
     code_base64 = base64.b64encode(code.encode()).decode()
 
-    params: dict[str, str] = {}
-    if image_type == 'pdf':
+    params: dict[str, str | bool] = {}
+    if kwargs.get('image_type') == 'pdf':
         url = f'https://mermaid.ink/pdf/{code_base64}'
-        if pdf_fit:
-            params['fit'] = ''
-        if pdf_landscape:
-            params['landscape'] = ''
-        if pdf_paper:
+        if kwargs.get('pdf_fit'):
+            params['fit'] = True
+        if kwargs.get('pdf_landscape'):
+            params['landscape'] = True
+        if pdf_paper := kwargs.get('pdf_paper'):
             params['paper'] = pdf_paper
-    elif image_type == 'svg':
+    elif kwargs.get('image_type') == 'svg':
         url = f'https://mermaid.ink/svg/{code_base64}'
     else:
         url = f'https://mermaid.ink/img/{code_base64}'
 
-        if image_type:
+        if image_type := kwargs.get('image_type'):
             params['type'] = image_type
 
-    if bg_color:
-        params['bgColor'] = bg_color
-    if theme:
+    if background_color := kwargs.get('background_color'):
+        params['bgColor'] = background_color
+    if theme := kwargs.get('theme'):
         params['theme'] = theme
-    if width:
+    if width := kwargs.get('width'):
         params['width'] = str(width)
-    if height:
+    if height := kwargs.get('height'):
         params['height'] = str(height)
-    if scale:
+    if scale := kwargs.get('scale'):
         params['scale'] = str(scale)
 
     response = httpx.get(url, params=params)
@@ -154,65 +184,26 @@ def request_image(
 def save_image(
     path: Path | str,
     graph: Graph[Any, Any],
-    start_node_ids: set[str],
-    *,
-    highlighted_nodes: Sequence[NodeIdent] | None = None,
-    highlight_css: str = 'fill:#f9f,stroke:#333,stroke-width:4px',
-    image_type: ImageType | None = None,
-    pdf_fit: bool = False,
-    pdf_landscape: bool = False,
-    pdf_paper: PdfPaper | None = None,
-    bg_color: str | None = None,
-    theme: Theme | None = None,
-    width: int | None = None,
-    height: int | None = None,
-    scale: Annotated[float, Ge(1), Le(3)] | None = None,
+    start_nodes: Sequence[NodeIdent] | NodeIdent,
+    /,
+    **kwargs: Unpack[MermaidConfig],
 ) -> None:
     """Generate an image of a Mermaid diagram using [mermaid.ink](https://mermaid.ink) and save it to a local file.
 
     Args:
         path: The path to save the image to.
         graph: The graph to generate the image for.
-        start_node_ids: IDs of start nodes of the graph.
-        highlighted_nodes: Identifiers of nodes to highlight.
-        highlight_css: CSS to use for highlighting nodes.
-        image_type: The image type to generate. If unspecified, the default behavior is `'jpeg'`.
-        pdf_fit: When using image_type='pdf', whether to fit the diagram to the PDF page.
-        pdf_landscape: When using image_type='pdf', whether to use landscape orientation for the PDF.
-            This has no effect if using `pdf_fit`.
-        pdf_paper: When using image_type='pdf', the paper size of the PDF.
-        bg_color: The background color of the diagram. If None, the default transparent background is used.
-            The color value is interpreted as a hexadecimal color code by default (and should not have a leading '#'),
-            but you can also use named colors by prefixing the value with `'!'`.
-            For example, valid choices include `bg_color='!white'` or `bg_color='FF0000'`.
-        theme: The theme of the diagram. Defaults to 'default'.
-        width: The width of the diagram.
-        height: The height of the diagram.
-        scale: The scale of the diagram. The scale must be a number between 1 and 3, and you can only set
-            a scale if one or both of width and height are set.
+        start_nodes: Identifiers of nodes that start the graph.
+        **kwargs: Additional parameters to configure mermaid chart generation.
     """
     if isinstance(path, str):
         path = Path(path)
 
-    if image_type is None:
-        ext = path.suffix.lower()
+    if 'image_type' not in kwargs:
+        ext = path.suffix.lower()[1:]
         # no need to check for .jpeg/.jpg, as it is the default
-        if ext in {'.png', '.webp', '.svg', '.pdf'}:
-            image_type = cast(ImageType, ext[1:])
+        if ext in ('png', 'webp', 'svg', 'pdf'):
+            kwargs['image_type'] = ext
 
-    image_data = request_image(
-        graph,
-        start_node_ids,
-        highlighted_nodes=highlighted_nodes,
-        highlight_css=highlight_css,
-        image_type=image_type,
-        pdf_fit=pdf_fit,
-        pdf_landscape=pdf_landscape,
-        pdf_paper=pdf_paper,
-        bg_color=bg_color,
-        theme=theme,
-        width=width,
-        height=height,
-        scale=scale,
-    )
-    Path(path).write_bytes(image_data)
+    image_data = request_image(graph, start_nodes, **kwargs)
+    path.write_bytes(image_data)
