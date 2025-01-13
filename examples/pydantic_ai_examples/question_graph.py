ecc243435dc66e0274e0e3f9c06f6c7552a373bb
Samuel Colvin <s@muelcolvin.com>
2025-01-13 14:32:04 +0000

    extending graph docs

--- examples/pydantic_ai_examples/question_graph.py
+++ examples/pydantic_ai_examples/question_graph.py
@@ -8,9 +8,11 @@ Run with:
 from __future__ import annotations as _annotations
 
 from dataclasses import dataclass, field
+from pathlib import Path
 from typing import Annotated
 
 import logfire
+import pydantic
 from devtools import debug
 from pydantic_graph import BaseNode, Edge, End, Graph, GraphContext, HistoryStep
 
@@ -26,43 +28,30 @@ ask_agent = Agent('openai:gpt-4o', result_type=str)
 
 @dataclass
 class QuestionState:
+    question: str | None = None
     ask_agent_messages: list[ModelMessage] = field(default_factory=list)
     evaluate_agent_messages: list[ModelMessage] = field(default_factory=list)
 
 
 @dataclass
 class Ask(BaseNode[QuestionState]):
-    """Generate a question to ask the user.
-
-    Uses the GPT-4o model to generate the question.
-    """
-
-    async def run(
-        self, ctx: GraphContext[QuestionState]
-    ) -> Annotated[Answer, Edge(label='ask the question')]:
+    async def run(self, ctx: GraphContext[QuestionState]) -> Answer:
         result = await ask_agent.run(
             'Ask a simple question with a single correct answer.',
             message_history=ctx.state.ask_agent_messages,
         )
         ctx.state.ask_agent_messages += result.all_messages()
-        return Answer(result.data)
+        ctx.state.question = result.data
+        return Answer()
 
 
 @dataclass
 class Answer(BaseNode[QuestionState]):
-    """Get the answer to the question from the user.
-
-    This node must be completed outside the graph run.
-    """
-
-    question: str
     answer: str | None = None
 
-    async def run(
-        self, ctx: GraphContext[QuestionState]
-    ) -> Annotated[Evaluate, Edge(label='answer the question')]:
+    async def run(self, ctx: GraphContext[QuestionState]) -> Evaluate:
         assert self.answer is not None
-        return Evaluate(self.question, self.answer)
+        return Evaluate(self.answer)
 
 
 @dataclass
@@ -75,34 +64,31 @@ evaluate_agent = Agent(
     'openai:gpt-4o',
     result_type=EvaluationResult,
     system_prompt='Given a question and answer, evaluate if the answer is correct.',
-    result_tool_name='evaluation',
 )
 
 
 @dataclass
 class Evaluate(BaseNode[QuestionState]):
-    question: str
     answer: str
 
     async def run(
         self,
         ctx: GraphContext[QuestionState],
-    ) -> Congratulate | Castigate:
+    ) -> Congratulate | Reprimand:
+        assert ctx.state.question is not None
         result = await evaluate_agent.run(
-            format_as_xml({'question': self.question, 'answer': self.answer}),
+            format_as_xml({'question': ctx.state.question, 'answer': self.answer}),
             message_history=ctx.state.evaluate_agent_messages,
         )
         ctx.state.evaluate_agent_messages += result.all_messages()
         if result.data.correct:
             return Congratulate(result.data.comment)
         else:
-            return Castigate(result.data.comment)
+            return Reprimand(result.data.comment)
 
 
 @dataclass
 class Congratulate(BaseNode[QuestionState, None]):
-    """Congratulate the user and end."""
-
     comment: str
 
     async def run(
@@ -113,26 +99,23 @@ class Congratulate(BaseNode[QuestionState, None]):
 
 
 @dataclass
-class Castigate(BaseNode[QuestionState]):
-    """Castigate the user, then ask another question."""
-
+class Reprimand(BaseNode[QuestionState]):
     comment: str
 
-    async def run(
-        self, ctx: GraphContext[QuestionState]
-    ) -> Annotated[Ask, Edge(label='try again')]:
+    async def run(self, ctx: GraphContext[QuestionState]) -> Ask:
         print(f'Comment: {self.comment}')
+        # > Comment: Vichy is no longer the capital of France.
+        ctx.state.question = None
         return Ask()
 
 
-question_graph = Graph(nodes=(Ask, Answer, Evaluate, Congratulate, Castigate))
-print(question_graph.mermaid_code(start_node=Ask, notes=False))
+question_graph = Graph(nodes=(Ask, Answer, Evaluate, Congratulate, Reprimand))
 
 
-async def main():
+async def run_as_continuous():
     state = QuestionState()
     node = Ask()
-    history: list[HistoryStep[QuestionState]] = []
+    history: list[HistoryStep[QuestionState, None]] = []
     with logfire.span('run questions graph'):
         while True:
             node = await question_graph.next(state, node, history)
@@ -140,11 +123,70 @@ async def main():
                 debug([e.data_snapshot() for e in history])
                 break
             elif isinstance(node, Answer):
-                node.answer = input(f'{node.question} ')
+                assert state.question
+                node.answer = input(f'{state.question} ')
             # otherwise just continue
+
+
+ta = pydantic.TypeAdapter(
+    list[Annotated[HistoryStep[QuestionState, None], pydantic.Discriminator('kind')]]
+)
+
+
+async def run_as_cli(answer: str | None):
+    history_file = Path('question_graph_history.json')
+    if history_file.exists():
+        history = ta.validate_json(history_file.read_bytes())
+    else:
+        history = []
+
+    if history:
+        last = history[-1]
+        assert last.kind != 'node', 'expected last step to be a node'
+        state = last.state
+        assert answer is not None, 'answer is required to continue from history'
+        node = Answer(answer)
+    else:
+        state = QuestionState()
+        node = Ask()
+
+    with logfire.span('run questions graph'):
+        while True:
+            node = await question_graph.next(state, node, history)
+            if isinstance(node, End):
+                debug([e.data_snapshot() for e in history])
+                print('Finished!')
+                break
+            elif isinstance(node, Answer):
+                break
+            # otherwise just continue
+
+    history_file.write_bytes(ta.dump_json(history))
 
 
 if __name__ == '__main__':
     import asyncio
+    import sys
 
-    asyncio.run(main())
+    try:
+        sub_command = sys.argv[1]
+        assert sub_command in ('continuous', 'cli', 'mermaid')
+    except (IndexError, AssertionError):
+        print(
+            'Usage:\n'
+            '  uv run -m pydantic_ai_examples.question_graph meriad\n'
+            'or:\n'
+            '  uv run -m pydantic_ai_examples.question_graph continuous\n'
+            'or:\n'
+            '  uv run -m pydantic_ai_examples.question_graph cli [answer]',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    if sub_command == 'mermaid':
+        print(question_graph.mermaid_code(start_node=Ask))
+    elif sub_command == 'continuous':
+        asyncio.run(run_as_continuous())
+    else:
+        a = sys.argv[2] if len(sys.argv) > 2 else None
+        asyncio.run(run_as_cli(a))
