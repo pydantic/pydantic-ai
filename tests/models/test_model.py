8557e7ccbff27522f0beeacf12a11e47ce2a6928
David Montague <35119617+dmontagu@users.noreply.github.com>
2025-01-26 10:54:52 -0700

    Clean up known model names

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -1,9 +1,18 @@
+from collections.abc import Iterator
 from importlib import import_module
+from typing import Any, get_args
 
 import pytest
 
 from pydantic_ai import UserError
-from pydantic_ai.models import infer_model
+from pydantic_ai.models import KnownModelName, infer_model
+from pydantic_ai.models.anthropic import AnthropicModelName
+from pydantic_ai.models.cohere import CohereModelName
+from pydantic_ai.models.gemini import GeminiModelName
+from pydantic_ai.models.groq import GroqModelName
+from pydantic_ai.models.mistral import MistralModelName
+from pydantic_ai.models.ollama import OllamaModelName
+from pydantic_ai.models.openai import OpenAIModelName
 
 from ..conftest import TestEnv
 
@@ -89,3 +98,41 @@ def test_infer_model(
 def test_infer_str_unknown():
     with pytest.raises(UserError, match='Unknown model: foobar'):
         infer_model('foobar')  # pyright: ignore[reportArgumentType]
+
+
+def test_known_model_names():
+    def get_model_names(model_name_type: Any) -> Iterator[str]:
+        for arg in get_args(model_name_type):
+            if isinstance(arg, str):
+                yield arg
+            else:
+                yield from get_model_names(arg)
+
+    anthropic_names = [f'anthropic:{n}' for n in get_model_names(AnthropicModelName)] + [
+        n for n in get_model_names(AnthropicModelName) if n.startswith('claude')
+    ]
+    cohere_names = [f'cohere:{n}' for n in get_model_names(CohereModelName)]
+    google_names = [f'google-gla:{n}' for n in get_model_names(GeminiModelName)] + [
+        f'google-vertex:{n}' for n in get_model_names(GeminiModelName)
+    ]
+    groq_names = [f'groq:{n}' for n in get_model_names(GroqModelName)]
+    mistral_names = [f'mistral:{n}' for n in get_model_names(MistralModelName)]
+    ollama_names = [f'ollama:{n}' for n in get_model_names(OllamaModelName)]
+    openai_names = [f'openai:{n}' for n in get_model_names(OpenAIModelName)] + [
+        n for n in get_model_names(OpenAIModelName) if n.startswith('o1') or n.startswith('gpt')
+    ]
+    extra_names = ['test']
+
+    generated_names = sorted(
+        anthropic_names
+        + cohere_names
+        + google_names
+        + groq_names
+        + mistral_names
+        + ollama_names
+        + openai_names
+        + extra_names
+    )
+    known_model_names = sorted(get_args(KnownModelName))
+
+    assert generated_names == known_model_names
