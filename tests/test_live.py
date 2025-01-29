a3c23b08e4e56f458a33b7a94dca180cdcf4fe91
sydney-runkle <sydneymarierunkle@gmail.com>
2025-01-29 14:44:56 -0500

    try r model for live test

--- tests/test_live.py
+++ tests/test_live.py
@@ -69,7 +69,7 @@ def mistral(http_client: httpx.AsyncClient, _tmp_path: Path) -> Model:
 def cohere(http_client: httpx.AsyncClient, _tmp_path: Path) -> Model:
     from pydantic_ai.models.cohere import CohereModel
 
-    return CohereModel('command')
+    return CohereModel('command-r')
 
 
 params = [
