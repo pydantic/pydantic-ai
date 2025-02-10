8c334623d12494eb7e161badff339643407d64a1
Alex Hall <alex.mojaki@gmail.com>
2025-02-10 16:49:38 +0200

    fallback model_name

--- pydantic_ai_slim/pydantic_ai/models/instrumented.py
+++ pydantic_ai_slim/pydantic_ai/models/instrumented.py
@@ -106,7 +106,7 @@ class InstrumentedModel(WrapperModel):
                     for k, v in {
                         # TODO finish_reason (https://github.com/open-telemetry/semantic-conventions/issues/1277), id
                         #  https://github.com/pydantic/pydantic-ai/issues/886
-                        'gen_ai.response.model': response.model_name,
+                        'gen_ai.response.model': response.model_name or model_name,
                         'gen_ai.usage.input_tokens': usage.request_tokens,
                         'gen_ai.usage.output_tokens': usage.response_tokens,
                     }.items()
