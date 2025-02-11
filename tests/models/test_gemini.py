a4b4ebb7efee9e953f530531af6b095a63b9482f
sydney-runkle <sydneymarierunkle@gmail.com>
2025-02-11 13:22:20 -0500

    fixing test

--- tests/models/test_gemini.py
+++ tests/models/test_gemini.py
@@ -14,6 +14,7 @@ from pydantic import BaseModel, Field
 from typing_extensions import Literal, TypeAlias
 
 from pydantic_ai import Agent, ModelRetry, UnexpectedModelBehavior, UserError
+from pydantic_ai.exceptions import ModelStatusError
 from pydantic_ai.messages import (
     ModelRequest,
     ModelResponse,
@@ -622,10 +623,10 @@ async def test_unexpected_response(client_with_handler: ClientWithHandler, env: 
     m = GeminiModel('gemini-1.5-flash', http_client=gemini_client)
     agent = Agent(m, system_prompt='this is the system prompt')
 
-    with pytest.raises(UnexpectedModelBehavior) as exc_info:
+    with pytest.raises(ModelStatusError) as exc_info:
         await agent.run('Hello')
 
-    assert str(exc_info.value) == snapshot('Unexpected response from gemini 401, body:\ninvalid request')
+    assert str(exc_info.value) == snapshot('status_code: 401, model_name: gemini-1.5-flash, body: invalid request')
 
 
 async def test_stream_text(get_gemini_client: GetGeminiClient):
