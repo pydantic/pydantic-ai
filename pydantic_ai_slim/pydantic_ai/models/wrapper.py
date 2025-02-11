c431cc675db002535d7bc82d2f9f63851575f477
Alex Hall <alex.mojaki@gmail.com>
2025-02-11 14:33:22 +0200

    test_instrumented_model

--- pydantic_ai_slim/pydantic_ai/models/wrapper.py
+++ pydantic_ai_slim/pydantic_ai/models/wrapper.py
@@ -1,11 +1,9 @@
-from collections.abc import AsyncIterator
-from contextlib import asynccontextmanager
 from dataclasses import dataclass
 from typing import Any
 
 from ..messages import ModelResponse
 from ..usage import Usage
-from . import Model, StreamedResponse
+from . import Model
 
 
 @dataclass
@@ -16,11 +14,6 @@ class WrapperModel(Model):
 
     async def request(self, *args: Any, **kwargs: Any) -> tuple[ModelResponse, Usage]:
         return await self.wrapped.request(*args, **kwargs)
-
-    @asynccontextmanager
-    async def request_stream(self, *args: Any, **kwargs: Any) -> AsyncIterator[StreamedResponse]:
-        async with self.wrapped.request_stream(*args, **kwargs) as stream:
-            yield stream
 
     def __getattr__(self, item: str):
         return getattr(self.wrapped, item)
