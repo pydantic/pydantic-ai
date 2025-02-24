89847292eeebb3faf48f1e374f809634d88d947a
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-02-24 13:02:50 -0300

    Update main.py
    Co-authored-by: David Montague <35119617+dmontagu@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import httpx
 
 from pydantic_ai import Agent, BinaryContent
 
-response = httpx.get('https://iili.io/3Hs4FMg.png')
-image_bytes = response.content
+image_response = httpx.get('https://iili.io/3Hs4FMg.png')
 
-image_content = BinaryContent(data=image_bytes, media_type='image/png')
+image_content = BinaryContent(data=image_response.content, media_type='image/png')
 
 agent = Agent(model='openai:gpt-4o')
 result = agent.run_sync(['What company is this logo from?', image_content])
