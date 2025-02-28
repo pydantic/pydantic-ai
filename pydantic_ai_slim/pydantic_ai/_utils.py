e51bd1247921cf523bab451ccc8eacc196244fa6
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-02-28 10:52:16 -0300

    Update pydantic_ai_slim/pydantic_ai/_utils.py

--- pydantic_ai_slim/pydantic_ai/_utils.py
+++ pydantic_ai_slim/pydantic_ai/_utils.py
@@ -49,7 +49,7 @@ def check_object_json_schema(schema: JsonSchemaValue) -> ObjectJsonSchema:
     if schema.get('type') == 'object':
         return schema
     elif schema.get('$ref') is not None:
-        return schema.get('$defs', {}).get(schema['$ref'].split('/')[-1])
+        return schema.get('$defs', {}).get(schema['$ref'][8:])  # This removes the initial "#/$defs/".
     else:
         raise UserError('Schema must be an object')
 
