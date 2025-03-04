7036f493fd2b921d7e573468413cd25d6ca05e98
Alex Hall <alex.mojaki@gmail.com>
2025-03-04 17:06:17 +0200

    update __all__

--- pydantic_ai_slim/pydantic_ai/__init__.py
+++ pydantic_ai_slim/pydantic_ai/__init__.py
@@ -11,6 +11,7 @@ from .exceptions import (
     UserError,
 )
 from .messages import AudioUrl, BinaryContent, ImageUrl
+from .models.instrumented import InstrumentationOptions
 from .tools import RunContext, Tool
 
 __all__ = (
@@ -34,6 +35,8 @@ __all__ = (
     'ImageUrl',
     'AudioUrl',
     'BinaryContent',
+    # models.instrumented
+    'InstrumentationOptions',
     # tools
     'Tool',
     'RunContext',
