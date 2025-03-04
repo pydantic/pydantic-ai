eabc0828eaedaa7311af732f13f08079891aa24c
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-03-04 16:51:44 +0100

    Add provider

--- tests/providers/test_provider_names.py
+++ tests/providers/test_provider_names.py
@@ -6,23 +6,23 @@ from pydantic_ai.providers import Provider, infer_provider
 
 from ..conftest import try_import
 
+test_inter_provider_params = []
 with try_import() as imports_successful:
     from pydantic_ai.providers.deepseek import DeepSeekProvider
     from pydantic_ai.providers.google_gla import GoogleGLAProvider
     from pydantic_ai.providers.google_vertex import GoogleVertexProvider
     from pydantic_ai.providers.openai import OpenAIProvider
 
-pytestmark = pytest.mark.skipif(not imports_successful(), reason='need to install all extra packages')
-
-
-@pytest.mark.parametrize(
-    ('provider, provider_cls'),
-    [
+    test_infer_provider_params = [
         ('deepseek', DeepSeekProvider),
         ('openai', OpenAIProvider),
         ('google-vertex', GoogleVertexProvider),
         ('google-gla', GoogleGLAProvider),
-    ],
-)
+    ]
+
+pytestmark = pytest.mark.skipif(not imports_successful(), reason='need to install all extra packages')
+
+
+@pytest.mark.parametrize(('provider, provider_cls'), test_infer_provider_params)
 def test_infer_provider(provider: str, provider_cls: type[Provider[Any]]):
     assert isinstance(infer_provider(provider), provider_cls)
