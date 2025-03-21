7768cc2820f0a5c2e1b37eb44307299bd5490a23
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-03-21 12:24:16 +0000

    Use `get_running_loop` instead of `get_event_loop`

--- pydantic_graph/pydantic_graph/_utils.py
+++ pydantic_graph/pydantic_graph/_utils.py
@@ -12,7 +12,7 @@ from typing_inspection.introspection import is_union_origin
 
 def get_event_loop():
     try:
-        event_loop = asyncio.get_event_loop()
+        event_loop = asyncio.get_running_loop()
     except RuntimeError:
         event_loop = asyncio.new_event_loop()
         asyncio.set_event_loop(event_loop)
