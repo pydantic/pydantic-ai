971ae7185235f088f7abf4c096b009e78bd26978
Alex Hall <alex.mojaki@gmail.com>
2025-03-25 17:09:08 +0200

    Move into try_import

--- tests/evals/test_otel.py
+++ tests/evals/test_otel.py
@@ -3,12 +3,12 @@ from __future__ import annotations as _annotations
 import asyncio
 
 import pytest
-from logfire.testing import CaptureLogfire
 
 from ..conftest import try_import
 
 with try_import() as imports_successful:
     import logfire
+    from logfire.testing import CaptureLogfire
 
     from pydantic_evals.otel._context_in_memory_span_exporter import (
         context_subtree,
