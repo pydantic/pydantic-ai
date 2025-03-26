b5fd7d2ae7f6e25d17a90e51f558a9430d41d4ab
Samuel Colvin <s@muelcolvin.com>
2025-03-26 10:57:38 +0000

    debug redirects

--- docs-site/src/index.ts
+++ docs-site/src/index.ts
@@ -3,6 +3,7 @@ export default {
     const r = await env.ASSETS.fetch(request)
     if (r.status == 404) {
       const url = new URL(request.url)
+      console.log({url, pathname: url.pathname})
       const redirectPath = redirect(url.pathname)
       if (redirectPath) {
         url.pathname = redirectPath
@@ -10,16 +11,16 @@ export default {
       }
       url.pathname = '/404.html'
       const r = await env.ASSETS.fetch(url)
-      return new Response(r.body, { status: 404 })
+      return new Response(r.body, { status: 404, headers: {'content-type': 'text/html'} })
     }
     return r
   },
 } satisfies ExportedHandler<Env>
 
 const redirect_lookup: Record<string, string> = {
-  '/common_tools/': '/common-tools/',
+  '/common_tools': '/common-tools/',
 }
 
 function redirect(pathname: string): string | null {
-  return redirect_lookup[pathname] ?? null
+  return redirect_lookup[pathname.replace(/\/+$/, '')] ?? null
 }
