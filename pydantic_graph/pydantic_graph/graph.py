fb82b3f3781d71b27ccb8eb4b326d4453fd61028
Samuel Colvin <s@muelcolvin.com>
2025-03-28 15:27:41 +0000

    typecheck evals

--- pydantic_graph/pydantic_graph/graph.py
+++ pydantic_graph/pydantic_graph/graph.py
@@ -111,7 +111,7 @@ class Graph(Generic[StateT, DepsT, RunEndT]):
         self.auto_instrument = auto_instrument
 
         parent_namespace = _utils.get_parent_namespace(inspect.currentframe())
-        self.node_defs: dict[str, NodeDef[StateT, DepsT, RunEndT]] = {}
+        self.node_defs = {}
         for node in nodes:
             self._register_node(node, parent_namespace)
 
