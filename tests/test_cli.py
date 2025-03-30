c02ecd55146ae3d6e4b87aa405dcdde30db35f96
harsha <harshav17@gmail.com>
2025-03-29 22:36:58 -0700

    additional space

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,7 +23,7 @@ def test_cli_help(capfd: CaptureFixture[str]):
 
     assert capfd.readouterr().out.splitlines() == snapshot(
         [
-            'usage: pai [-h] [--model [MODEL]] [--list-models] [--no-stream]',
+            'usage: pai [-h] [--model [MODEL]] [--list-models] [--no-stream] ',
             '[--version] [prompt]',
             '',
             IsStr(),
