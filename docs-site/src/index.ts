b0e70e6a1681bc7eb17b6440bd8bc59e46b7f4ba
Samuel Colvin <s@muelcolvin.com>
2025-04-15 16:00:53 +0100

    Add Changelog

--- docs-site/src/index.ts
+++ docs-site/src/index.ts
@@ -1,8 +1,14 @@
+ import { marked } from 'marked'
+
 export default {
   async fetch(request, env): Promise<Response> {
+    const url = new URL(request.url)
+    if (url.pathname === '/changelog.html') {
+      const changelog = await getChangelog()
+      return new Response(changelog, { headers: {'content-type': 'text/html'} })
+    }
     const r = await env.ASSETS.fetch(request)
     if (r.status == 404) {
-      const url = new URL(request.url)
       const redirectPath = redirect(url.pathname)
       if (redirectPath) {
         url.pathname = redirectPath
@@ -24,4 +30,50 @@ const redirect_lookup: Record<string, string> = {
 
 function redirect(pathname: string): string | null {
   return redirect_lookup[pathname.replace(/\/+$/, '')] ?? null
+}
+
+async function getChangelog(): Promise<string> {
+  const headers = {
+    'X-GitHub-Api-Version': '2022-11-28',
+    'User-Agent': 'pydantic-ai-docs'
+  }
+  let url: string = 'https://api.github.com/repos/pydantic/pydantic-ai/releases'
+  const releases: Release[] = []
+  while (typeof url === 'string') {
+    const response = await fetch(url, { headers })
+    if (!response.ok) {
+      const text = await response.text()
+      throw new Error(`Failed to fetch changelog: ${response.status} ${response.statusText} ${text}`)
+    }
+    const newReleases = await response.json() as Release[]
+    releases.push(...newReleases)
+    const linkHeader = response.headers.get('link')
+    if (!linkHeader) break
+    const nextUrl = linkHeader.match(/<([^>]+)>; rel="next"/)?.[1]
+    if (!nextUrl) break
+    url = nextUrl
+  }
+  console.log(releases.length)
+  return marked(releases.map(prepRelease).join('\n\n'))
+}
+
+interface Release {
+  name: string
+  body: string
+  html_url: string
+}
+
+function prepRelease(release: Release): string {
+  // console.log(release.body)
+  const body = release.body
+    .replace(/(#+)/g, (m) => `##${m}`)
+    .replace(/https:\/\/github.com\/pydantic\/pydantic-ai\/pull\/(\d+)/g, (url, id) => `[#${id}](${url})`)
+    .replace(/\*\*Full Changelog\*\*: (\S+)/, (_, url) => `[Compare diff](${url})`)
+  return `
+### ${release.name}
+
+${body}
+
+[View on GitHub](${release.html_url})
+`
 }
---
+++ docs/changelog.md
@@ -0,0 +1,31 @@
+# Changelog
+
+## Breaking Changes
+
+PydanticAI is still pre-version 1, so breaking changes will occur, however:
+
+- We try to minimize them as much as possible.
+- We will use minor version bumps to signify breaking changes.
+- Wherever possible we'll deprecate old features so code continues to work with deprecation warnings when changing the public API.
+- We intend to release V1 in summer 2025, and then follow strict semantic versioning, e.g. no intentional breaking changes except in major versions.
+
+### V0.1.0
+
+See [#1248](https://github.com/pydantic/pydantic-ai/pull/1248) — the attribute/parameter name `result` was renamed to `output` in many places. Hopefully all changes keep a deprecated attribute or parameter with the old name, so you should get many deprecation warnings.
+
+See [#1484](https://github.com/pydantic/pydantic-ai/pull/1484) — `format_as_xml` was moved and made available to import from the package root, e.g. `from pydantic_ai import format_as_xml`.
+
+## Full Changelog
+
+<div id="display-changelog">
+  For the full changelog, see <a href="https://github.com/pydantic/pydantic-ai/releases">GitHub Releases</a>.
+</div>
+
+<script>
+  async function getChangelog() {
+    const fullChangelog = document.getElementById('display-changelog');
+    const r = await fetch('/changelog.html');
+    fullChangelog.innerHTML = await r.text();
+  }
+  getChangelog();
+</script>
