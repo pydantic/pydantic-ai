d8cc89e4a287de5a76208a6c1f0ff87388c81a04
Zach Deane-Mayer <zach.mayer@gmail.com>
2025-04-21 09:42:26 -1000

    fix broken test

--- pydantic_ai_slim/pydantic_ai/models/__init__.py
+++ pydantic_ai_slim/pydantic_ai/models/__init__.py
@@ -194,6 +194,8 @@ KnownModelName = TypeAliasType(
         'o1-mini-2024-09-12',
         'o1-preview',
         'o1-preview-2024-09-12',
+        'o3',
+        'o3-2025-04-16',
         'o3-mini',
         'o3-mini-2025-01-31',
         'openai:chatgpt-4o-latest',
