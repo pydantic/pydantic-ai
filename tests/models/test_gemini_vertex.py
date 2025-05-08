8333ffab63ad3d99ad6eada474f45ad778ede13a
vricciardulli <61703242+vricciardulli@users.noreply.github.com>
2025-05-08 23:17:29 +0200

    add a cassette for `AudioUrl` test case

--- tests/models/test_gemini_vertex.py
+++ tests/models/test_gemini_vertex.py
@@ -1,16 +1,22 @@
-from __future__ import annotations as _annotations
+import os
 
-import json
-from pathlib import Path
-
-import httpx
 import pytest
+from inline_snapshot import Is, snapshot
 
 from pydantic_ai import Agent
-from pydantic_ai.messages import AudioUrl, DocumentUrl, ImageUrl, VideoUrl
+from pydantic_ai.messages import (
+    AudioUrl,
+    DocumentUrl,
+    ImageUrl,
+    ModelRequest,
+    ModelResponse,
+    TextPart,
+    UserPromptPart,
+    VideoUrl,
+)
 from pydantic_ai.models.gemini import GeminiModel
 
-from ..conftest import ClientWithHandler, TestEnv, try_import
+from ..conftest import IsDatetime, try_import
 
 with try_import() as imports_successful:
     from pydantic_ai.providers.google_vertex import GoogleVertexProvider
@@ -22,98 +28,93 @@ pytestmark = [
 ]
 
 
-async def mock_refresh_token():
-    return 'my-token'
-
-
+@pytest.mark.skipif(
+    not os.getenv('CI', False), reason='Requires properly configured local google vertex config to pass'
+)
 @pytest.mark.parametrize(
-    'url',
+    'url,expected_output',
     [
-        pytest.param(AudioUrl(url='https://cdn.openai.com/API/docs/audio/alloy.wav'), id='AudioUrl'),
-        pytest.param(AudioUrl(url='gs://my-bucket/path/to/audio-blob.mp3'), id='AudioUrl (gs://)'),
         pytest.param(
-            DocumentUrl(url='https://storage.googleapis.com/cloud-samples-data/generative-ai/pdf/2403.05530.pdf'),
-            id='DocumentUrl',
+            AudioUrl(url='https://cdn.openai.com/API/docs/audio/alloy.wav'),
+            'The URL discusses the phenomenon of the sun rising in the east and setting in the west, a basic observation known to humans for millennia.',
+            id='AudioUrl',
         ),
-        pytest.param(DocumentUrl(url='gs://my-bucket/path/to/pdf-blob.pdf'), id='DocumentUrl (gs://)'),
-        pytest.param(
-            ImageUrl(url='https://upload.wikimedia.org/wikipedia/commons/6/6a/Www.wikipedia_screenshot_%282021%29.png'),
-            id='ImageUrl',
-        ),
-        pytest.param(ImageUrl(url='gs://my-bucket/path/to/image-blob.png'), id='ImageUrl (gs://)'),
-        pytest.param(VideoUrl(url='https://data.grepit.app/assets/tiny_video.mp4'), id='VideoUrl'),
-        pytest.param(VideoUrl(url='gs://my-bucket/path/to/video-blob.mp4'), id='VideoUrl (gs://)'),
-        pytest.param(VideoUrl(url='https://youtu.be/lCdaVNyHtjU'), id='VideoUrl (YouTube)'),
+        # pytest.param(
+        #     DocumentUrl(url='https://storage.googleapis.com/cloud-samples-data/generative-ai/pdf/2403.05530.pdf'),
+        #     (
+        #         'The URL points to a report titled "Gemini 1.5: Unlocking multimodal understanding across millions of tokens of context". The report introduces the latest model in the Gemini family, Gemini 1.5 Pro, which is a highly'
+        #         ' compute-efficient multimodal mixture-of-experts model capable of recalling and reasoning over fine-grained information'
+        #         ' from millions of tokens of context, including multiple long documents and hours of video and audio. The report'
+        #         " compares Gemini 1.5 Pro's performance with Gemini 1.0 Pro and Ultra across several benchmarks. The model's"
+        #         ' responsible deployment, training, and evaluation processes are detailed.'
+        #     ),
+        #     id='DocumentUrl',
+        # ),
+        # pytest.param(
+        #     ImageUrl(url='https://upload.wikimedia.org/wikipedia/commons/6/6a/Www.wikipedia_screenshot_%282021%29.png'),
+        #     'The main content of the URL is the Wikipedia multilingual portal, showcasing the availability of Wikipedia in various languages, their respective article counts, and links to download the Wikipedia app.',
+        #     id='ImageUrl',
+        # ),
+        # pytest.param(
+        #     VideoUrl(url='https://data.grepit.app/assets/tiny_video.mp4'),
+        #     'The image shows a picturesque alley in a Greek island town, likely in the Cyclades. The alley is lined with traditional white-washed buildings, and has a restaurant or cafe setup with tables and chairs. The alley leads to a view of the blue Aegean Sea. The scene evokes a relaxing, vacation-like atmosphere.',
+        #     id='VideoUrl',
+        # ),
+        # pytest.param(
+        #     VideoUrl(url='https://youtu.be/lCdaVNyHtjU'),
+        #     (
+        #         'The main content of the URL is a discussion about analyzing recent 404 HTTP responses using Logfire.'
+        #         ' The analysis identifies patterns and trends in the 404 errors, including the most common endpoints returning 404s,'
+        #         ' request patterns, timeline-related issues, organization/project access problems, and configuration/authentication'
+        #         ' issues. The analysis also provides recommendations for addressing the 404 errors.'
+        #     ),
+        #     id='VideoUrl (YouTube)',
+        # ),
+        # pytest.param(
+        #     AudioUrl(url='gs://pydantic-ai-dev/openai-alloy.wav'),
+        #     'The main content of the URL discusses the observation of the sun rising in the east and setting in the west, a phenomenon known for thousands of years.',
+        #     id='AudioUrl (gs)',
+        # ),
+        # pytest.param(
+        #     DocumentUrl(url='gs://pydantic-ai-dev/Gemini_1_5_Pro_Technical_Report_Arxiv_1805.pdf'),
+        #     """The main content of the URL is a research report titled "Gemini 1.5: Unlocking multimodal understanding across millions of tokens of context". The report introduces Gemini 1.5 Pro, a highly compute-efficient multimodal model capable of recalling and reasoning over fine-grained information from millions of tokens of context. It details the model's architecture, performance on various benchmarks, and evaluations of its long-context capabilities across modalities like text, video, and audio. The report also discusses responsible deployment aspects, such as impact assessment and safety evaluations.\n""",
+        #     id='DocumentUrl (gs)',
+        # ),
+        # pytest.param(
+        #     ImageUrl(url='gs://pydantic-ai-dev/wikipedia_screenshot.png'),
+        #     'The main content of the URL is the Wikipedia homepage, listing different languages and the number of articles available in each language. It also provides links to download the Wikipedia app, and other related projects.',
+        #     id='ImageUrl (gs)',
+        # ),
+        # pytest.param(
+        #     VideoUrl(url='gs://pydantic-ai-dev/grepit-tiny-video.mp4'),
+        #     'The image shows a picturesque outdoor dining area in a Greek island town. The tables and chairs are set up in a narrow, whitewashed alleyway leading towards the blue sea. The white buildings are typical of Cycladic architecture.',
+        #     id='VideoUrl (gs)',
+        # ),
     ],
 )
-async def test_url_inputs_are_sent_via_file_uri_field(
-    url: AudioUrl | DocumentUrl | ImageUrl | VideoUrl,
-    monkeypatch: pytest.MonkeyPatch,
-    tmp_path: Path,
-    client_with_handler: ClientWithHandler,
-    env: TestEnv,
-    allow_model_requests: None,
+@pytest.mark.vcr()
+async def test_url_input(
+    url: AudioUrl | DocumentUrl | ImageUrl | VideoUrl, expected_output: str, allow_model_requests: None
 ) -> None:
-    service_account_path = tmp_path / 'service_account.json'
-    save_service_account(service_account_path, 'my-project-id')
-
-    def handler(request: httpx.Request) -> httpx.Response:
-        text = json.loads(request.content)['contents'][0]['parts'][0]['text']
-        assert text == 'What is the main content of this URL?'
-        file_data = json.loads(request.content)['contents'][0]['parts'][1]['fileData']
-        assert file_data == {
-            'fileUri': url.url,
-            'mimeType': url.media_type,
-        }
-
-        return httpx.Response(
-            200,
-            content=json.dumps({'candidates': [{'content': {'role': 'user', 'parts': [{'text': '...'}]}}]}),
-            headers={'Content-Type': 'application/json'},
-        )
-
-    gemini_client = client_with_handler(handler)
-    provider = GoogleVertexProvider(http_client=gemini_client, service_account_file=service_account_path)
-    monkeypatch.setattr(provider.client.auth, '_refresh_token', mock_refresh_token)
-
-    m = GeminiModel('gemini-1.5-flash', provider=provider)
-    agent = Agent(m)
+    provider = GoogleVertexProvider(project_id='pydantic-ai', region='us-central1')
+    agent = Agent(model=GeminiModel('gemini-2.0-flash', provider=provider))
     result = await agent.run(['What is the main content of this URL?', url])
 
-    assert result.output == '...'
-
-
-def save_service_account(service_account_path: Path, project_id: str) -> None:
-    service_account = {
-        'type': 'service_account',
-        'project_id': project_id,
-        'private_key_id': 'abc',
-        # this is just a random private key I created with `openssl genpke ...`, it doesn't do anything
-        'private_key': (
-            '-----BEGIN PRIVATE KEY-----\n'
-            'MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAMFrZYX4gZ20qv88\n'
-            'jD0QCswXgcxgP7Ta06G47QEFprDVcv4WMUBDJVAKofzVcYyhsasWsOSxcpA8LIi9\n'
-            '/VS2Otf8CmIK6nPBCD17Qgt8/IQYXOS4U2EBh0yjo0HQ4vFpkqium4lLWxrAZohA\n'
-            '8r82clV08iLRUW3J+xvN23iPHyVDAgMBAAECgYBScRJe3iNxMvbHv+kOhe30O/jJ\n'
-            'QiUlUzhtcEMk8mGwceqHvrHTcEtRKJcPC3NQvALcp9lSQQhRzjQ1PLXkC6BcfKFd\n'
-            '03q5tVPmJiqsHbSyUyHWzdlHP42xWpl/RmX/DfRKGhPOvufZpSTzkmKWtN+7osHu\n'
-            '7eiMpg2EDswCvOgf0QJBAPXLYwHbZLaM2KEMDgJSse5ZTE/0VMf+5vSTGUmHkr9c\n'
-            'Wx2G1i258kc/JgsXInPbq4BnK9hd0Xj2T5cmEmQtm4UCQQDJc02DFnPnjPnnDUwg\n'
-            'BPhrCyW+rnBGUVjehveu4XgbGx7l3wsbORTaKdCX3HIKUupgfFwFcDlMUzUy6fPO\n'
-            'IuQnAkA8FhVE/fIX4kSO0hiWnsqafr/2B7+2CG1DOraC0B6ioxwvEqhHE17T5e8R\n'
-            '5PzqH7hEMnR4dy7fCC+avpbeYHvVAkA5W58iR+5Qa49r/hlCtKeWsuHYXQqSuu62\n'
-            'zW8QWBo+fYZapRsgcSxCwc0msBm4XstlFYON+NoXpUlsabiFZOHZAkEA8Ffq3xoU\n'
-            'y0eYGy3MEzxx96F+tkl59lfkwHKWchWZJ95vAKWJaHx9WFxSWiJofbRna8Iim6pY\n'
-            'BootYWyTCfjjwA==\n'
-            '-----END PRIVATE KEY-----\n'
-        ),
-        'client_email': 'testing-pydantic-ai@pydantic-ai.iam.gserviceaccount.com',
-        'client_id': '123',
-        'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-        'token_uri': 'https://oauth2.googleapis.com/token',
-        'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-        'client_x509_cert_url': 'https://www.googleapis.com/...',
-        'universe_domain': 'googleapis.com',
-    }
-
-    service_account_path.write_text(json.dumps(service_account, indent=2))
+    assert result.output == snapshot(Is(expected_output))
+    assert result.all_messages() == snapshot(
+        [
+            ModelRequest(
+                parts=[
+                    UserPromptPart(
+                        content=['What is the main content of this URL?', Is(url)],
+                        timestamp=IsDatetime(),
+                    ),
+                ]
+            ),
+            ModelResponse(
+                parts=[TextPart(content=Is(expected_output))],
+                model_name='gemini-2.0-flash',
+                timestamp=IsDatetime(),
+            ),
+        ]
+    )
