7dcfda8de416b45e8b4fc19e34051318eed11c43
vricciardulli <61703242+vricciardulli@users.noreply.github.com>
2025-05-23 11:42:10 +0200

    try to use a fixture to patch `google-auth` package

--- tests/models/test_gemini_vertexai.py
+++ tests/models/test_gemini_vertexai.py
@@ -1,7 +1,9 @@
 import os
+from dataclasses import dataclass
 
 import pytest
 from inline_snapshot import snapshot
+from pytest_mock import MockerFixture
 
 from pydantic_ai import Agent
 from pydantic_ai.models.gemini import GeminiModel, GeminiModelSettings
@@ -9,7 +11,7 @@ from pydantic_ai.models.gemini import GeminiModel, GeminiModelSettings
 from ..conftest import try_import
 
 with try_import() as imports_successful:
-    from pydantic_ai.providers.google_vertex import GoogleVertexProvider
+    from google.auth.transport.requests import Request
 
 
 pytestmark = [
@@ -18,13 +20,28 @@ pytestmark = [
 ]
 
 
+@pytest.fixture(autouse=True)
+def vertex_provider_auth(mocker: MockerFixture) -> None:  # pragma: lax no cover
+    # Locally, we authenticate via `gcloud` CLI, so we don't need to patch anything.
+    if not os.getenv('CI'):
+        return
+
+    @dataclass
+    class NoOpCredentials:
+        token = 'my-token'
+
+        def refresh(self, request: Request): ...
+
+    return_value = (NoOpCredentials(), 'pydantic-ai')
+    mocker.patch('pydantic_ai.providers.google_vertex.google.auth.default', return_value=return_value)
+
+
 @pytest.mark.skipif(
     not os.getenv('CI', False), reason='Requires properly configured local google vertex config to pass'
 )
 @pytest.mark.vcr()
 async def test_labels(allow_model_requests: None) -> None:
-    provider = GoogleVertexProvider(project_id='pydantic-ai', region='us-central1')
-    m = GeminiModel('gemini-2.0-flash', provider=provider)
+    m = GeminiModel('gemini-2.0-flash', provider='google-vertex')
     agent = Agent(m)
 
     result = await agent.run(
