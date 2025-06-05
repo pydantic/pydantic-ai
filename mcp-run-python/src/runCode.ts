bc220d57e72cb670b0788979fa499088a241c242
Samuel Colvin <s@muelcolvin.com>
2025-06-05 09:18:27 -0700

    remove console.log shim

--- mcp-run-python/src/runCode.ts
+++ mcp-run-python/src/runCode.ts
@@ -13,11 +13,6 @@ export async function runCode(
   files: CodeFile[],
   log: (level: LoggingLevel, data: string) => void,
 ): Promise<RunSuccess | RunError> {
-  // remove once https://github.com/pyodide/pyodide/pull/5514 is released
-  const realConsoleLog = console.log
-  // deno-lint-ignore no-explicit-any
-  console.log = (...args: any[]) => log('debug', args.join(' '))
-
   const output: string[] = []
   const pyodide = await loadPyodide({
     stdout: (msg) => {
@@ -90,7 +85,6 @@ export async function runCode(
   }
   sys.stdout.flush()
   sys.stderr.flush()
-  console.log = realConsoleLog
   return runResult
 }
 
