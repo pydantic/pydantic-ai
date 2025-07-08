4daa15283fc0fee999364b9bcc2cabec208a42bf
Douwe Maan <hi@douwe.me>
2025-07-08 22:48:25 +0000

    Branch is OK to not be covered

--- tests/test_a2a.py
+++ tests/test_a2a.py
@@ -332,7 +332,7 @@ async def test_a2a_multiple_messages():
 
             task = None
             tries = 0
-            while tries < 10:
+            while tries < 10:  # pragma: no branch
                 await anyio.sleep(0.1)
                 task = await a2a_client.get_task(task_id)
                 tries += 1
