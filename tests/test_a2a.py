39e035369716ba4aaa064dac00542d95092b3fbc
Douwe Maan <hi@douwe.me>
2025-07-09 19:57:00 +0000

    Branch is OK to not be covered

--- tests/test_a2a.py
+++ tests/test_a2a.py
@@ -336,7 +336,7 @@ async def test_a2a_multiple_messages():
                 await anyio.sleep(0.1)
                 task = await a2a_client.get_task(task_id)
                 tries += 1
-                if 'result' in task and task['result']['status']['state'] == 'completed':
+                if 'result' in task and task['result']['status']['state'] == 'completed':  # pragma: no branch
                     break
 
             assert task == snapshot(
