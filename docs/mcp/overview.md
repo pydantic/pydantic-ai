c91cd9f4be4a46a1016c4ebbae53f66a1f8e05cc
Samuel Colvin <s@muelcolvin.com>
2025-09-02 15:35:32 -0700

    remove more references

--- docs/mcp/overview.md
+++ docs/mcp/overview.md
@@ -17,4 +17,4 @@ Some examples of what this means:
 
 - Pydantic AI could use a web search service implemented as an MCP server to implement a deep research agent
 - Cursor could connect to the [Pydantic Logfire](https://github.com/pydantic/logfire-mcp) MCP server to search logs, traces and metrics to gain context while fixing a bug
-- Pydantic AI, or any other MCP client could connect to our [Run Python](run-python.md) MCP server to run arbitrary Python code in a sandboxed environment
+- Pydantic AI, or any other MCP client could connect to our [Run Python](https://github.com/pydantic/mcp-run-python) MCP server to run arbitrary Python code in a sandboxed environment
--- pydantic_ai_slim/pydantic_ai/mcp.py
+++ pydantic_ai_slim/pydantic_ai/mcp.py
@@ -400,16 +400,7 @@ class MCPServerStdio(MCPServer):
     from pydantic_ai.mcp import MCPServerStdio
 
     server = MCPServerStdio(  # (1)!
-        'deno',
-        args=[
-            'run',
-            '-N',
-            '-R=node_modules',
-            '-W=node_modules',
-            '--node-modules-dir=auto',
-            'jsr:@pydantic/mcp-run-python',
-            'stdio',
-        ]
+        'uv', args=['run', 'mcp-run-python', 'stdio'],
     )
     agent = Agent('openai:gpt-4o', toolsets=[server])
 
@@ -418,7 +409,7 @@ class MCPServerStdio(MCPServer):
             ...
     ```
 
-    1. See [MCP Run Python](../mcp/run-python.md) for more information.
+    1. See [MCP Run Python](https://github.com/pydantic/mcp-run-python) for more information.
     2. This will start the server as a subprocess and connect to it.
     """
 
@@ -739,7 +730,7 @@ class MCPServerSSE(_MCPServerHTTP):
             ...
     ```
 
-    1. E.g. you might be connecting to a server run with [`mcp-run-python`](../mcp/run-python.md).
+    1. E.g. you might be connecting to a server run with [`mcp-run-python`](https://github.com/pydantic/mcp-run-python).
     2. This will connect to a server running on `localhost:3001`.
     """
 
@@ -764,7 +755,7 @@ class MCPServerHTTP(MCPServerSSE):
     from pydantic_ai import Agent
     from pydantic_ai.mcp import MCPServerHTTP
 
-    server = MCPServerHTTP('http://localhost:3001/sse')  # (1)!
+    server = MCPServerHTTP('http://localhost:3001/sse')
     agent = Agent('openai:gpt-4o', toolsets=[server])
 
     async def main():
@@ -772,8 +763,7 @@ class MCPServerHTTP(MCPServerSSE):
             ...
     ```
 
-    1. E.g. you might be connecting to a server run with [`mcp-run-python`](../mcp/run-python.md).
-    2. This will connect to a server running on `localhost:3001`.
+    1. This will connect to a server running on `localhost:3001`.
     """
 
 
