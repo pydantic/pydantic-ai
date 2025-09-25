cea33633053349bb81d35f7f281e240d92c6782a
igalshilman <igal@restate.dev>
2025-09-25 16:18:21 +0200

    Apply formatting and lint

--- pydantic_ai_slim/pydantic_ai/durable_exec/restate/_model.py
+++ pydantic_ai_slim/pydantic_ai/durable_exec/restate/_model.py
@@ -1,22 +1,18 @@
-from typing import Any, Optional
+from typing import Any
+
+from restate import Context, RunOptions
 
 from pydantic_ai.durable_exec.restate._serde import PydanticTypeAdapter
 from pydantic_ai.messages import ModelResponse
 from pydantic_ai.models import Model
 from pydantic_ai.models.wrapper import WrapperModel
 
-from restate import Context, RunOptions
 
 class RestateModelWrapper(WrapperModel):
-    
-    def __init__(self,
-                 wrapped: Model,
-                 context: Context,
-                 max_attempts: Optional[int] = None):
+    def __init__(self, wrapped: Model, context: Context, max_attempts: int | None = None):
         super().__init__(wrapped)
         self.options = RunOptions[ModelResponse](serde=PydanticTypeAdapter(ModelResponse), max_attempts=max_attempts)
         self.context = context
-        
+
     async def request(self, *args: Any, **kwargs: Any) -> ModelResponse:
-        return await self.context.run_typed("Model call", self.wrapped.request, self.options, *args, **kwargs)
- 
+        return await self.context.run_typed('Model call', self.wrapped.request, self.options, *args, **kwargs)
--- pydantic_ai_slim/pydantic_ai/durable_exec/restate/_toolset.py
+++ pydantic_ai_slim/pydantic_ai/durable_exec/restate/_toolset.py
@@ -1,40 +1,44 @@
 from __future__ import annotations
+
+from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Callable, Any, Generic
+from typing import Any, Generic
+
+from restate import Context, RunOptions
 
 from pydantic_ai import ToolDefinition
-from pydantic_ai.tools import AgentDepsT, RunContext 
 from pydantic_ai._run_context import AgentDepsT
+from pydantic_ai.mcp import MCPServer, ToolResult
+from pydantic_ai.tools import RunContext
 from pydantic_ai.toolsets.abstract import AbstractToolset, ToolsetTool
-from pydantic_ai.toolsets.abstract import AbstractToolset
 from pydantic_ai.toolsets.wrapper import WrapperToolset
 
-from pydantic_ai.tools import AgentDepsT, RunContext 
-from pydantic_ai.mcp import MCPServer, ToolResult
-
-from restate import Context,  RunOptions 
-
 from ._serde import PydanticTypeAdapter
-  
+
+
 @dataclass
 class RestateContextRunResult:
     """A simple wrapper for tool results to be used with Restate's run_typed."""
+
     output: Any
-    
+
+
 @dataclass
 class RestateMCPGetToolsContextRunResult:
     """A simple wrapper for tool results to be used with Restate's run_typed."""
-    output: dict[str, ToolDefinition] 
-    
+
+    output: dict[str, ToolDefinition]
+
+
 @dataclass
 class RestateMCPToolRunResult(Generic[AgentDepsT]):
     """A simple wrapper for tool results to be used with Restate's run_typed."""
-    output: ToolResult 
+
+    output: ToolResult
+
 
 class RestateContextRunToolSet(WrapperToolset[AgentDepsT]):
-    """
-    A toolset that automatically wraps tool calls with restate's `ctx.run_typed()`.
-    """
+    """A toolset that automatically wraps tool calls with restate's `ctx.run_typed()`."""
 
     def __init__(self, wrapped: AbstractToolset[AgentDepsT], context: Context):
         super().__init__(wrapped)
@@ -44,14 +48,13 @@ class RestateContextRunToolSet(WrapperToolset[AgentDepsT]):
     async def call_tool(
         self, name: str, tool_args: dict[str, Any], ctx: RunContext[AgentDepsT], tool: ToolsetTool[AgentDepsT]
     ) -> Any:
-
         async def action() -> RestateContextRunResult:
             output = await self.wrapped.call_tool(name, tool_args, ctx, tool)
             return RestateContextRunResult(output=output)
 
-        res = await self._context.run_typed(f"Calling {name}", action, self.options)
+        res = await self._context.run_typed(f'Calling {name}', action, self.options)
         return res.output
-    
+
     def visit_and_replace(
         self, visitor: Callable[[AbstractToolset[AgentDepsT]], AbstractToolset[AgentDepsT]]
     ) -> AbstractToolset[AgentDepsT]:
@@ -61,17 +64,12 @@ class RestateContextRunToolSet(WrapperToolset[AgentDepsT]):
 class RestateMCPServer(WrapperToolset[AgentDepsT]):
     """A wrapper for MCPServer that integrates with restate."""
 
-    def __init__(
-        self,
-        wrapped: MCPServer,
-        context: Context
-    ):
+    def __init__(self, wrapped: MCPServer, context: Context):
         super().__init__(wrapped)
         self._wrapped = wrapped
         self._context = context
         self._mcp_tool_run_serde = PydanticTypeAdapter(RestateMCPToolRunResult[AgentDepsT])
         self._mcp_get_tools_serde = PydanticTypeAdapter(RestateMCPGetToolsContextRunResult)
-
 
     def visit_and_replace(
         self, visitor: Callable[[AbstractToolset[AgentDepsT]], AbstractToolset[AgentDepsT]]
@@ -79,7 +77,6 @@ class RestateMCPServer(WrapperToolset[AgentDepsT]):
         return visitor(self)
 
     async def get_tools(self, ctx: RunContext[AgentDepsT]) -> dict[str, ToolsetTool[AgentDepsT]]:
-        
         async def get_tools_in_context() -> RestateMCPGetToolsContextRunResult:
             res = await self._wrapped.get_tools(ctx)
             # ToolsetTool is not serializable as it holds a SchemaValidator (which is also the same for every MCP tool so unnecessary to pass along the wire every time),
@@ -88,7 +85,7 @@ class RestateMCPServer(WrapperToolset[AgentDepsT]):
 
         options = RunOptions(serde=self._mcp_get_tools_serde)
 
-        tool_defs = await self._context.run_typed("get mcp tools", get_tools_in_context, options) 
+        tool_defs = await self._context.run_typed('get mcp tools', get_tools_in_context, options)
 
         return {name: self.tool_for_tool_def(tool_def) for name, tool_def in tool_defs.output.items()}
 
@@ -108,6 +105,6 @@ class RestateMCPServer(WrapperToolset[AgentDepsT]):
             return RestateMCPToolRunResult(output=res)
 
         options = RunOptions(serde=self._mcp_tool_run_serde)
-        res = await self._context.run_typed(f"Calling mcp tool {name}", call_tool_in_context, options)
+        res = await self._context.run_typed(f'Calling mcp tool {name}', call_tool_in_context, options)
 
         return res.output
