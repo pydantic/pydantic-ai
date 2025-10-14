74f9989d601c0add39d947d637c1522bfcedacb2
RobinPicard <robin.picard@sciencespo.fr>
2025-10-14 17:12:24 +0200

    Add support for the Outlines model

--- pydantic_ai_slim/pydantic_ai/providers/__init__.py
+++ pydantic_ai_slim/pydantic_ai/providers/__init__.py
@@ -146,6 +146,10 @@ def infer_provider_class(provider: str) -> type[Provider[Any]]:  # noqa: C901
         from .nebius import NebiusProvider
 
         return NebiusProvider
+    elif provider == 'outlines':
+        from .outlines import OutlinesProvider
+
+        return OutlinesProvider
     else:  # pragma: no cover
         raise ValueError(f'Unknown provider: {provider}')
 
