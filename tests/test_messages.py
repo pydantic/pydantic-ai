6e0b72d3c1c1d48bab9f5f521f9c8a7d8a5beab3
David Sanchez <64162682+dsfaccini@users.noreply.github.com>
2025-12-16 00:04:54 -0500

    fix tests

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -609,7 +609,7 @@ def test_image_url_validation_with_optional_identifier():
     assert image_url_ta.dump_python(image) == snapshot(
         {
             'url': 'https://example.com/image.jpg',
-            'force_download': None,
+            'force_download': False,
             'vendor_metadata': None,
             'kind': 'image-url',
             'media_type': 'image/jpeg',
@@ -626,7 +626,7 @@ def test_image_url_validation_with_optional_identifier():
     assert image_url_ta.dump_python(image) == snapshot(
         {
             'url': 'https://example.com/image.jpg',
-            'force_download': None,
+            'force_download': False,
             'vendor_metadata': None,
             'kind': 'image-url',
             'media_type': 'image/png',
