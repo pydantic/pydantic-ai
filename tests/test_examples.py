caa8b1cd044998b3fbf9955c7f5e2f286cb58b21
David Sanchez <64162682+dsfaccini@users.noreply.github.com>
2025-12-19 10:12:35 -0500

    skip it

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -149,7 +149,6 @@ def test_docs_examples(
 
     mocker.patch('pydantic_ai.mcp.MCPServerSSE', return_value=MockMCPServer())
     mocker.patch('pydantic_ai.mcp.MCPServerStreamableHTTP', return_value=MockMCPServer())
-    mocker.patch('pydantic_ai.mcp.MCPServerStdio', return_value=MockMCPServer())
     mocker.patch('mcp.server.fastmcp.FastMCP')
 
     env.set('OPENAI_API_KEY', 'testing')
