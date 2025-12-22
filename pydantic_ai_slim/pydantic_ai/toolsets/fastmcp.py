00cdff586affdd75750896761d15061e135c12b3
adtyavrdhn <adtyavrdhn@gmail.com>
2025-12-22 10:59:38 +0530

    adding get_all_tool_definitions in fastmcp_toolset

--- pydantic_ai_slim/pydantic_ai/toolsets/fastmcp.py
+++ pydantic_ai_slim/pydantic_ai/toolsets/fastmcp.py
@@ -173,9 +173,21 @@ class FastMCPToolset(AbstractToolset[AgentDepsT]):
             args_validator=TOOL_SCHEMA_VALIDATOR,
         )
 
-    # async def get_all_tool_definitions(self, ctx: RunContext[AgentDepsT]) -> list[ToolDefinition]:
-    #     """The list of tool definitions available(including deferred ones)."""
-    #     return [tool_def for tool_def in (await super().get_all_tool_definitions(ctx))]
+    async def get_all_tool_definitions(self, ctx: RunContext[AgentDepsT]) -> list[ToolDefinition]:
+        """The list of tool definitions available(including deferred ones)."""
+        return [
+            ToolDefinition(
+                name=mcp_tool.name,
+                description=mcp_tool.description,
+                parameters_json_schema=mcp_tool.inputSchema,
+                metadata={
+                    'meta': mcp_tool.meta,
+                    'annotations': mcp_tool.annotations.model_dump() if mcp_tool.annotations else None,
+                    'output_schema': mcp_tool.outputSchema or None,
+                },
+            )
+            for mcp_tool in await self.client.list_tools()
+        ]
 
 
 def _map_fastmcp_tool_results(parts: list[ContentBlock]) -> list[FastMCPToolResult] | FastMCPToolResult:
