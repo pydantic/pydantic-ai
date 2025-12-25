0e39a8630694d78ac306ddddf8aec02ad80a1ca4
Danipulok <danipulok@gmail.com>
2025-12-25 13:51:18 +0200

    Add empty line

--- tests/models/test_openai.py
+++ tests/models/test_openai.py
@@ -203,6 +203,7 @@ async def test_request_simple_success(allow_model_requests: None):
             ),
         ]
     )
+    
     assert get_mock_chat_completion_kwargs(mock_client) == [
         {
             'messages': [{'content': 'hello', 'role': 'user'}],
