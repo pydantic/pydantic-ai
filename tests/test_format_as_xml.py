0b06f0db2564adc95da9f722b1f0bb8befd1ef7e
Bohdan <63935457+bohdanhr@users.noreply.github.com>
2026-01-31 17:26:14 +0400

    add uuid

--- tests/test_format_as_xml.py
+++ tests/test_format_as_xml.py
@@ -5,6 +5,7 @@ from datetime import date, datetime, time, timedelta
 from decimal import Decimal
 from enum import Enum
 from typing import Any
+from uuid import UUID
 
 import pytest
 from inline_snapshot import snapshot
@@ -625,11 +626,17 @@ def test_custom_null():
 
 
 def test_non_primitive_types():
-    class ExamplePydanticModelWithDecimal(BaseModel):
+    class ExamplePydanticModelWithNonPrimitiveTypes(BaseModel):
         name: str
         age: Decimal
+        uuid: UUID
 
-    assert format_as_xml(ExamplePydanticModelWithDecimal(name='John', age=Decimal('123.45'))) == snapshot("""\
+    assert format_as_xml(
+        ExamplePydanticModelWithNonPrimitiveTypes(
+            name='John', age=Decimal('123.45'), uuid=UUID('123e4567-e89b-12d3-a456-426614174000')
+        )
+    ) == snapshot("""\
 <name>John</name>
-<age>123.45</age>\
+<age>123.45</age>
+<uuid>123e4567-e89b-12d3-a456-426614174000</uuid>\
 """)
