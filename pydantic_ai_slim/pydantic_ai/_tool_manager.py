5efb908a6d6d6729e5a7484f85b215b803b3ce5f
jaiwinshah3 <jaiwin2000.js@gmail.com>
2026-03-20 13:53:17 -0700

    let span be unset

--- pydantic_ai_slim/pydantic_ai/_tool_manager.py
+++ pydantic_ai_slim/pydantic_ai/_tool_manager.py
@@ -430,8 +430,9 @@ class ToolManager(Generic[AgentDepsT]):
                     except (TypeError, ValueError):
                         metadata_str = repr(exc.metadata)
                     span.set_attribute(instrumentation_names.tool_deferral_metadata_attr, metadata_str)
-                span.record_exception(exc, escaped=True)
-                span.set_status(StatusCode.ERROR)
+                if instrumentation_version < 5:
+                    span.record_exception(exc, escaped=True)
+                    span.set_status(StatusCode.ERROR)
                 raise
             except ToolRetryError as e:
                 part = e.tool_retry
